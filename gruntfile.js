faf4c19e9240194e749275646614cd90fe611f0d
Jamie Webster <Jamie Webster>
2016-12-09 00:46:13 -0500

    Update grunt to add pre and post build clean steps

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,26 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         ts: {
-            options: {
-                target: 'es5',
-                sourceMap: true,
-                comments: true
-            },
             default : {
-                src: ["**/*.ts", "!node_modules/**/*.ts", "!typings/**/*.ts"],
-                outDir: "dist"
+                src: ["app/**/*.ts"],
+                outDir: 'dist/',
+                options: {
+                    target: 'es5',
+                    sourceMap: false,
+                }
 			}
         },
-        clean: ["bin/*"]
+        clean: {
+            preBuild: {
+                src: ["dist/*"]
+            },
+            postBuild: {
+                src: ['app/.baseDir.ts', 'dist/.baseDir.js']
+            }
+        }
     });
 
-    grunt.registerTask("build", ['clean', 'ts']);
+    grunt.registerTask("build", ['clean:preBuild', 'ts:default', 'clean:postBuild']);
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks("grunt-ts");
