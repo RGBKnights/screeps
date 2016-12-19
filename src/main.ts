8b20b25e546989ed8ab9096f53128c9e7dd1ee92
Jamie Webster <jamie.webster@akirasystems.com>
2016-12-19 12:54:13 -0500

    refacted hive

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,6 @@
 /// <reference path="../typings/index.d.ts" />
 "use strict";
 
-// import * as _ from "lodash";
-
 let hive = require("hive");
 
-module.exports.loop = function () {
-
-    // Cleanup hive
-    hive.reset();
-    hive.createWorkers(5);
-    hive.processTowers();
-    hive.processWorkers();
-};
+module.exports.loop = hive.loop();
