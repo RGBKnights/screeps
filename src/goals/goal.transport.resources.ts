b4d7bdfb43a984df871567754642df7c1f734d2d
Jamie Webster <Jamie Webster>
2016-12-31 14:55:49 -0500

    updated header

--- src/goals/goal.transport.resources.ts
+++ src/goals/goal.transport.resources.ts
@@ -1,5 +1,9 @@
 /// <reference path="../../typings/index.d.ts" />
-"use strict";
+/**
+ * The long description of the file's purpose goes here and
+ * describes in detail the complete functionality of the file.
+ * This description can span several lines and ends with a period.
+ */
 
 module.exports = {
 };
---
+++ src/tasks/task.wait.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../typings/index.d.ts" />
+/**
+ * The long description of the file's purpose goes here and
+ * describes in detail the complete functionality of the file.
+ * This description can span several lines and ends with a period.
+ */
+
+module.exports = {
+};
