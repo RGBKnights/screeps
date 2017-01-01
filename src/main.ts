fc149b34904c7628041e539bd5c155d3e2862912
Jamie Webster <Jamie Webster>
2017-01-01 12:09:41 -0500

    Test

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+// import * as CreepManager from "./components/creeps/creepManager";
+// import * as Config from "./config/config";
+// import { log } from "./components/support/log";
+
+// Any code written outside the `loop()` method is executed only when the
+// Screeps system reloads your script.
+// Use this bootstrap wisely. You can cache some of your stuff to save CPU.
+// You should extend prototypes before the game loop executes here.
+
+
+/**
+ * Screeps system expects this "loop" method in main.js to run the
+ * application. If we have this line, we can be sure that the globals are
+ * bootstrapped properly and the game loop is executed.
+ * http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
+ *
+ * @export
+ */
+export function loop() {
+  return;
+}
