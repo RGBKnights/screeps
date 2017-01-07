1896581bfeb963aa83c359d1ca3be5f0bd53e496
Jamie Webster <Jamie Webster@DESKTOP-563HJ3N>
2017-01-07 09:39:04 -0500

    basic idea

--- Screeps.Hive/main.js
+++ Screeps.Hive/main.js
@@ -1,20 +1,26 @@
 "use strict";
-
-// Any code written outside the `loop()` method is executed only when the
-// Screeps system reloads your script.
-// Use this bootstrap wisely. You can cache some of your stuff to save CPU.
-// You should extend prototypes before the game loop executes here.
+/**
+ * Any code written outside the `loop()` method is executed only when the
+ * application loads. Use this bootstrap wisely. 
+ * You can cache some of your stuff to save CPU.
+ * You should extend prototypes before the game loop executes here.
+ */
 
 
 /**
  * Screeps system expects this "loop" method in main.js to run the
- * application. If we have this line, we can be sure that the globals are
+ * application. If we have this line, we can be sure that the global are
  * bootstrapped properly and the game loop is executed.
  * http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
- *
- * @export
  */
  
 exports.loop = function () {
+    // Test: caterpillar movement system 
+    // [
+    //  WORK(only at the ends?[how to get there?]),
+    //  CARRY(links in chain), 
+    //  MOVE(needed? how much?)
+    // ]
+
     return;
 };
