b6516638a85ce8fd589607bea4567d662610135a
Jamie Webster <Jamie Webster@DESKTOP-563HJ3N>
2017-01-12 16:53:32 -0500

    Convertted to vsial studio

--- Screeps.Hive/main.js
+++ Screeps.Hive/main.js
@@ -13,7 +13,31 @@
  * bootstrapped properly and the game loop is executed.
  * http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
  */
- 
+
+function setup() {
+    /*
+    * Specify whether to use this new experimental pathfinder in game objects methods. 
+    * This method should be invoked every tick. It affects the following methods behavior: 
+    * Room.findPath, RoomPosition.findPathTo, RoomPosition.findClosestByPath, Creep.moveTo
+    * http://support.screeps.com/hc/en-us/articles/207023879
+    */
+
+    PathFinder.use(true);
+}
+
+function freeMemory() {
+    /*
+    * Removes creeps from memory that are no longer living.
+    * http://support.screeps.com/hc/en-us/community/posts/201107272-Small-snippet-to-clear-the-memory-of-dead-creeps-
+    */
+
+    for (var i in Memory.creeps) {
+        if (!Game.creeps[i]) {
+            delete Memory.creeps[i];
+        }
+    }
+}
+
 exports.loop = function () {
     // Test: caterpillar movement system 
     // [
@@ -22,5 +46,37 @@ exports.loop = function () {
     //  MOVE(needed? how much?)
     // ]
 
-    return;
+    setup();
+
+    freeMemory();
+
+    for (var i in Game.rooms) {
+        var room = Game.rooms[i];
+        if (room.controller.my) {
+            if (!room.memory.processed) {
+                var spawns = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
+                var sources = room.find(FIND_SOURCES);
+
+                var pathNest = room.findPath(spawns[0].pos, sources[1].pos);
+                pathNest.pop();
+                pathNest.shift();
+
+                for (var i in pathNest) {
+                    var step = pathNest[i];
+                    room.createFlag(step.x, step.y, i, COLOR_RED);
+                }
+
+                var pathNest = room.findPath(room.controller.pos, sources[0].pos);
+                pathNest.pop();
+                pathNest.shift();
+
+                for (var i in pathNest) {
+                    var step = pathNest[i];
+                    room.createFlag(step.x, step.y, i, COLOR_BLUE);
+                }
+
+                room.memory.processed = true;
+            }
+        }
+    }
 };
