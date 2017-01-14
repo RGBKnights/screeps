fbaa4f3c359d2cb92527bc3eb7d71d2dbf6cbf2c
Jamie Webster <Jamie Webster>
2017-01-14 12:05:14 -0500

    reverted to starter

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,47 @@
-/// <reference path="../typings/index.d.ts" />
-"use strict";
+// import * as CreepManager from "./components/creeps/creepManager";
+import * as Config from "./config/config";
+// import { log } from "./components/support/log";
 
-// import * as _ from "lodash";
+// Any code written outside the `loop()` method is executed only when the
+// Screeps system reloads your script.
+// Use this bootstrap wisely. You can cache some of your stuff to save CPU.
+// You should extend prototypes before the game loop executes here.
 
-let hive = require("hive");
+// This is an example for using a config variable from `config.ts`.
+if (Config.USE_PATHFINDER) {
+  PathFinder.use(true);
+}
 
-module.exports.loop = function () {
+// log.info("load");
 
-    // Cleanup hive
-    hive.reset();
-    hive.createWorkers(5);
-    hive.processTowers();
-    hive.processWorkers();
-};
+/**
+ * Screeps system expects this "loop" method in main.js to run the
+ * application. If we have this line, we can be sure that the globals are
+ * bootstrapped properly and the game loop is executed.
+ * http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
+ *
+ * @export
+ */
+export function loop() {
+
+  for (let i in Game.rooms) {
+    let room: Room = Game.rooms[i];
+
+    // CreepManager.run(room);
+
+    gcDeadCreepsMemory(room);
+  }
+}
+
+function gcDeadCreepsMemory(room: Room) {
+  // Clears any non-existing creep memory.
+  for (let name in Memory.creeps) {
+    let creep: any = Memory.creeps[name];
+
+    if (creep.room === room.name) {
+      if (!Game.creeps[name]) {
+        delete Memory.creeps[name];
+      }
+    }
+  }
+}
